a36e4014fbfa271b0f1089fc96dda1a799a5cd16
Alex Wilkerson <alex@uncorrected.com>
2016-08-13 17:38:28 -0500

    added styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var apikey = "cf6408f51645e3e591770caea53ce720";
@@ -5,6 +6,8 @@ if (navigator.geolocation) {
         var longitude = position.coords.longitude;
 
         var weather;
+        // this was built before I was using jQuery which seems to make
+        // things significantly easier.
         var xhr = new XMLHttpRequest();
         var url = "http://api.openweathermap.org/data/2.5/weather?apikey=" + apikey + "&lat=" + latitude + "&lon=" + longitude;
         xhr.onreadystatechange = function() {
@@ -13,7 +16,7 @@ if (navigator.geolocation) {
                 weather = JSON.parse(xhr.responseText);
                 locationAvailable(weather);
             }
-        }
+        };
         xhr.open("GET", url, true);
         xhr.send();
    }, showError);
@@ -25,40 +28,63 @@ if (navigator.geolocation) {
 // site is able to locate the user
 // the weather variable is the JSON object
 function locationAvailable(weather){
+    const FAHRENHEIT = "&deg;F";
+    const CELSIUS = "&deg;C";
     console.log(weather); 
-    var weathericon = document.getElementById("weather-icon");
-    var temp = parseFloat(kelvinToFahrenheit(weather.main.temp).toFixed(2));
-    var docHTML = temp + "&deg;F<br>";
-    docHTML += "<i class=\"wi wi-owm-" + weather.weather[0].id + "\"></i>";
-    docHTML += " " + weather.weather[0].description;
-    weathericon.innerHTML = docHTML;
+    var tempF = parseFloat(kelvinToFahrenheit(weather.main.temp).toFixed(0));
+    var tempC = parseFloat(kelvinToCelsius(weather.main.temp).toFixed(0));
+    var desc = weather.weather[0].description;
+    var tempHtml = "<i class=\"wi wi-owm-" + weather.weather[0].id + "\"></i>";
+    var degreeType = "F";
+    $(".weather-icon").html(tempHtml);
+    $(".temp").html(tempF);
+    $(".degrees").html(FAHRENHEIT);
+    $(".description").html(desc);
+
+    $(".degrees").on("click", convertDegrees);
+
+    function convertDegrees(){
+      console.log("clicked");
+      if (degreeType === "F") {
+        $(".temp").html(tempC);
+        $(".degrees").html(CELSIUS);
+        degreeType = "C";
+      } else {
+        $(".temp").html(tempF);
+        $(".degrees").html(FAHRENHEIT);
+        degreeType = "F";
+      }
+    }
 }
 
 function kelvinToFahrenheit(k){
     return (k * 1.8) - 459.67;
 }
 
+function kelvinToCelsius(k){
+  return k-273.15;
+}
+
 // This is the function where all of the js goes if the
 // site is unable to locate the user
 function locationUnavailable() {
-    document.body.innerText = "Geolocation is not supported by this browser.";
+    $(".weather").html("Geolocation is not supported by this browser.");
 }
 
-function showError(error)
-  {
-  switch(error.code) 
-    {
+function showError(error) {
+  switch(error.code) {
     case error.PERMISSION_DENIED:
-      document.body.innerHTML="User denied the request for Geolocation."
+      $(".weather").html("User denied the request for Geolocation.");
       break;
     case error.POSITION_UNAVAILABLE:
-      document.body.innerHTML="Location information is unavailable."
+      $(".weather").html("Location information is unavailable.");
       break;
     case error.TIMEOUT:
-      document.body.innerHTML="The request to get user location timed out."
+      $(".weather").html("The request to get user location timed out.");
       break;
     case error.UNKNOWN_ERROR:
-      document.body.innerHTML="An unknown error occurred."
+      $(".weather").html("An unknown error occurred.");
       break;
-    }
   }
+}
+});
